b62831f14b05d219df4b7f13191abbab911da9ce
leiyong <794166169@qq.com>
2025-05-10 12:30:18 +0800

    结算校验

--- jeecg-boot/jeecg-module-rider/src/main/java/org/jeecg/modules/rider/interview/service/impl/RiderInterviewServiceImpl.java
+++ jeecg-boot/jeecg-module-rider/src/main/java/org/jeecg/modules/rider/interview/service/impl/RiderInterviewServiceImpl.java
@@ -60,10 +60,10 @@ public class RiderInterviewServiceImpl extends ServiceImpl<RiderInterviewMapper,
             throw new JeecgBootException("站点为空，不能结算！");
         }
         if(riderInterviews.stream().anyMatch(s-> Objects.equals(s.getPassStatus() , 0))){
-            throw new JeecgBootException("骑手未入职，不能结算！");
+            throw new JeecgBootException("不能选择未入职的记录！");
         }
         if(riderInterviews.stream().anyMatch(s-> Objects.equals(s.getSettleStatus() , 1))){
-            throw new JeecgBootException("骑手已经结算，不能重复结算！");
+            throw new JeecgBootException("不能选择已结算的记录！");
         }
         List<String> siteIdList = riderInterviews.stream().map(x -> x.getSiteId()).collect(Collectors.toList());
 
